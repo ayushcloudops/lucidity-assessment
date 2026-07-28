7471132eec58a2841ee7aad64dc9a90b42ca7fa0
Ayush Kumar Singh <53579697+ayushcloudops@users.noreply.github.com>
2026-07-29 00:33:52 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,16 +1,36 @@
-from flask import Flask, jsonify
-import socket
+from flask import Flask
+from prometheus_client import Counter, Histogram, generate_latest, CONTENT_TYPE_LATEST
+import time
 
 app = Flask(__name__)
 
+# Metrics
+REQUEST_COUNT = Counter(
+    "http_requests_total",
+    "Total HTTP Requests",
+    ["method", "endpoint", "status"]
+)
+
+REQUEST_LATENCY = Histogram(
+    "http_request_duration_seconds",
+    "HTTP Request Latency"
+)
+
 @app.route("/")
-def index():
-    return "Hello World", 200
+@REQUEST_LATENCY.time()
+def home():
+    REQUEST_COUNT.labels("GET", "/", "200").inc()
+    return "Hello from EKS!"
 
 @app.route("/health")
 def health():
-    return jsonify({
-        "status": "healthy",
-        "application": "hello-world",
-        "hostname": socket.gethostname()
-    }), 200
+    return "OK"
+
+@app.route("/metrics")
+def metrics():
+    return generate_latest(), 200, {
+        "Content-Type": CONTENT_TYPE_LATEST
+    }
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
